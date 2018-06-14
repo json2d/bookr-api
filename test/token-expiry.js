5baed115ab5d46c3a047a3294fdf35b1eb732446
Jason Yung <json.yung@gmail.com>
2018-06-14 17:06:38 -0400

    Create tests for token refresh per request

--- test/token-expiry.js
+++ test/token-expiry.js
@@ -11,7 +11,7 @@ describe('token behavior', function() {
     User.remove({}, done)
   })
 
-  let token;
+  let token, tokenA, tokenB;
 
   const expiresIn = Number(process.env.JWT_EXPIRE_IN_SECS);
 
@@ -44,4 +44,41 @@ describe('token behavior', function() {
         .expect(401)
 
     }).timeout((expiresIn + 2)*1000); //this test will timeout in the time the token expires + 2 more seconds
+
+    it(`refreshes after every successful request`, async function() {
+      await request(app)
+        .post('/auth/login')
+        .send({username:process.env.EMAIL_SENDER, password:"password" })
+        .expect(200)
+        .expect(hasWellformedToken)
+        .expect((res) => {
+          tokenA = res.body.token
+        })
+
+        console.log(`waiting ${expiresIn-1} seconds to refresh token...`)
+
+        await new Promise(res => setTimeout(res,(expiresIn-1)*1000))
+
+        await request(app)
+          .get('/books')
+          .set('Authorization',`Bearer ${tokenA}`)
+          .send()
+          .expect(200)
+          .expect(hasWellformedToken)
+          .expect((res) => {
+            tokenB = res.body.token
+          })
+
+        console.log(`waiting ${2} seconds to allow first token expires...`)
+
+        await new Promise(res => setTimeout(res,2000))
+
+        await request(app)
+          .get('/books')
+          .set('Authorization',`Bearer ${tokenB}`)
+          .send()
+          .expect(200)
+
+      }).timeout((expiresIn + 2)*1000); //this test will timeout in the time the token expires + 2 more seconds
+
 })
