d9f4378ec6d8dab59e1f0154d9cdf1aec54cf6a6
Jason Yung <json.yung@gmail.com>
2018-06-14 20:16:56 -0400

    Add generated API doc for GitHub Pages

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "bookr-api",
+  "version": "0.1.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-06-15T00:13:40.982Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
